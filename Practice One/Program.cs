76fb0e361f4258b5d134a8a6ce74a543299c16a8
andrew klima <andrewklima7@gmail.com>
2021-05-24 10:16:58 -0500

    practice repo

---
+++ Practice One/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test repo =)");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
